75f0409df8721483927b3692b220d15c727b7eca
Victor <victor.castanos@gmail.com>
2023-11-27 17:35:20 +0100

    ok

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <navBar className="navBar">
+      <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" alt="ironhack" />
+      <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" alt="menu-top" />
+
+
+    </navBar>
+    <div className="container">
+<h1>Say hello to ReactJS</h1>
+<h3>You will learn how to use the most popular frontend library, and become a super Ninja developer</h3>
+<button>THIS IS LA HOSTIA!</button>
+    </div>
+    
+    <footer className="footer">
+<img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png" alt="icon1" />
+<img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png" alt="icon2" />
+<img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png" alt="icon3" />
+<img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png" alt="icon4" />
+    </footer>
+    </div>
+  );
+}
+
+export default App;
+
